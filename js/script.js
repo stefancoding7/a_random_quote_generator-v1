33c8c9afdc3b6eca4978c4179596c58990795b1a
Stefan Caky <stefan.caky777@gmail.com>
2021-01-06 20:52:34 +0000

    finsihed and tested printQuote function

--- js/script.js
+++ js/script.js
@@ -9,24 +9,43 @@ project 1 - A Random Quote Generator
 
 /*** 
  * `quotes` array 
- *  @param {quote} quotes.quote Quote
- *  @param {source} quotes.source Who created the 
+ *  @typedef {Object} quotes
+ *  @property {quote} quotes.quote - Quote
+ *  @property {source} quotes.source - Who created the quote
+ * 
+ * 
 ***/
 const quotes = [
     {
       quote: 'The way to get started is to quit talking and begin doing.',
-      source: 'Walt Disney'
+      source: 'Walt Disney',
+      tags: ['#kids', '#play', '#humor']
     },
     {
       quote: 'If life were predictable it would cease to be life, and be without flavor.',
       source: 'Eleanor Roosevelt',
       citation: 'Song',
-      year: '1945'
+      year: '1945',
+      tags: ['#life', '#happy']
     },
     {
-      quotes: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough." ,
-      source: 'Oprah Winfrey'
+      quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough." ,
+      source: 'Oprah Winfrey',
+      tags: ['#politics', '#free', '#time']
     },
+    {
+      quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough." ,
+      source: 'Steve Jobs',
+      citation: 'Computing',
+      year: '1996',
+      tags: ['#computer', '#modern', '#mobile', '#apple'],
+    },
+    {
+      quote: "Curiosity about life in all of its aspects, I think, is still the secret of great creative people." ,
+      source: 'Leo Burnett',
+      tags: ['#humor', '#politics']
+    },
+   
 
 ];
 
@@ -36,12 +55,15 @@ const quotes = [
 /**
  * `getRandomQuote` function.
  *
- * @param {string} n - 
- * @return {string} A good string
- *    
+ * @param {string} n  - Return a random quote object using the random number variable  
+ * @return {string} Return the random object from array
+ * 
  */
+
  function getRandomQuote (n) {
+
     let random = Math.floor(Math.random() * n.length);
+    
      return n[random];
  }
 
@@ -49,13 +71,37 @@ const quotes = [
 
 /*
  * `printQuote` function
- *
- * @param {string} n - 
- * @return {string} A good string
  *    
+ *    
+ *    @return {string} A concatenated string and passed to quote-box class
  */
  function printQuote() {
-   
+      let quotePrint = getRandomQuote(quotes);
+      let tags = '';
+      let html = `<p class="quote">${quotePrint.quote}</p>
+      <p class="source">${quotePrint.source}
+      `;
+    
+      if(quotePrint.citation) {
+          html += `<span class="citation">${quotePrint.citation}</span>`;
+      }
+
+      if(quotePrint.year) {
+        html += `<span class="citation">${quotePrint.year}</span>`;
+      }
+
+      html += `</p>
+      <ul class="tag-items">`
+          for(let i = 0; i <= quotePrint.tags.length -1; i++) {
+             html += ` <li><a href="#" class="tags">${quotePrint.tags[i]}</a></li>`
+          }    
+          
+         
+      html += `</ul>`; // html variable concatenate all strings "quote", "source", "tags" ...
+      
+         
+      return document.getElementById('quote-box').innerHTML = html;
+     
  }
 
 
